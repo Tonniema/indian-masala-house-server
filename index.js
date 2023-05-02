fe1d27a9fc2f66e2d36c19e6e906fa2721b215f8
Tonniema <tonniemasarker@gmail.com>
2023-05-03 04:16:22 +0600

    recipe.json file added

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 5000;
+const  Recipe = require('./data/recipe.json');
+const chef = require('./data/chefInfo.json');
 
-const chefInfo = require('./data/chefInfo.json');
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 
@@ -11,10 +12,25 @@ app.get('/', (req, res) =>{
     res.send('Indian Masala is running')
 });
 
-app.get('/chefInfo', (req, res) =>{
-    res.send(chefInfo);
+app.get('/chef', (req, res) =>{
+    res.send(chef);
     
 })
+
+app.get('/chef/:id', (req, res) => {
+    const id = req.params.id
+    if (id == 0) {
+      res.send(Recipe)
+    } else {
+      const Chef_Recipe = Recipe.filter(r => r.Recipe_ID === id)
+      res.send(Chef_Recipe)
+    }
+  })
+  
+  
+  app.get('/Recipe', (req, res) => {
+    res.send(Recipe)
+  })
 
 app.listen(port, () => {
     console.log(`Indian Masala API is running on port: ${port}`)
