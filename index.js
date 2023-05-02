b2f356827fc9c6058c8df357dabe973697860391
Tonniema <tonniemasarker@gmail.com>
2023-05-02 17:55:05 +0600

    getting json data in localhost

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const chefInfo = require('./data/chefInfo.json');
+
 app.get('/', (req, res) =>{
     res.send('Indian Masala is running')
 });
 
 app.get('/chefInfo', (req, res) =>{
+    res.send(chefInfo);
     
 })
 
