15c4c800fbb7ccb8650ca52d6df345338139ffcd
Tonniema <tonniemasarker@gmail.com>
2023-05-02 17:43:44 +0600

    declaring port variable and creating server

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) =>{
+    res.send('Indian Masala is running')
+});
+
+app.get('/chefInfo', (req, res) =>{
+    
+})
+
+app.listen(port, () => {
+    console.log(`Indian Masala API is running on port: ${port}`)
+
+})
